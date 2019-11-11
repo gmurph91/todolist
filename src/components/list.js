c1cf7133e5c1d33f25aed5a1a165275b3bc48ef9
Gregory Murphy <murphygreg6@gmail.com>
2019-11-11 16:40:45 -0700

    added descriptions and due dates. debug

--- src/components/list.js
+++ src/components/list.js
@@ -6,8 +6,12 @@ export default class List extends Component{
     render(){
         return (
                 <div>
-                    <button className="List" onClick={this.props.clicked}>{this.props.name}</button>
-                    <input name={this.props.name} description={this.props.description} id={this.props.id} className={this.props.list} type="checkbox" checked={this.props.complete} onChange={this.props.completechange}/>
+                    <button className="List" id={this.props.id2} onClick={this.props.clicked}>{this.props.name}</button>
+                    <input name={this.props.name} data-description={this.props.description} data-duedate={this.props.duedate} id={this.props.id} data-list={this.props.list} type="checkbox" checked={this.props.complete} onChange={this.props.completechange}/>
+                    <div className="content" id={this.props.id2 + 1}>
+                    <p>{this.props.description}</p>
+                    <p>Due: {this.props.duedate}</p>
+                    </div>
                 </div>
         )
     }
--- src/pages/page.js
+++ src/pages/page.js
@@ -7,6 +7,9 @@ export default class Page extends Component {
         this.state = {
           complete: false,
           name: "",
+          duedate: "",
+          description: "",
+          list: "",
           default: false,
         };
     
@@ -17,23 +20,35 @@ export default class Page extends Component {
         const id = event.target.id;
         const target = event.target;
         var x = document.getElementById(`${id}`)
-        console.log(x.className)
          const value = target.type === 'checkbox' ? target.checked : target.value;
 
         this.setState({
       complete: value,
       name: event.target.name,
-      description: event.target.description,
+      description: x.dataset.description,
+      duedate: x.dataset.duedate,
       id: event.target.id,
-      list: x.className,
+      list: x.dataset.list,
     });
     await (this.setState)
     this.update(this.state.id)
-    console.log(this.state)
+    this.setState({
+        complete: false,
+        name: "",
+        duedate: "",
+        description: "",
+        list: "",
+    })
     }
 
-    clicked = () => {
-        console.log("this clicked")
+    clicked = (event) => {
+         const id = event.target.id;
+          var content = document.getElementById(`${id}`+1);
+          if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
     }
 
     renderLists = () => {
@@ -43,7 +58,9 @@ export default class Page extends Component {
         return unique.map((item, i) => {
             return (
             <div key={i} className="todolist">
+              <h2 className="listheader">{unique[i]}</h2>
                 <h2 className="listheader">To Do:</h2>
+            
                         {this.renderList(unique[i])}
                 <h2 className="listheader">Complete:</h2>
                         {this.renderList2(unique[i])}
@@ -57,7 +74,9 @@ export default class Page extends Component {
         try{
         let list = this.props.list.filter(toDo => toDo.complete === false && toDo.list === listID)
         return list.map((item, i) => {
-          return <List key={i} id={list[i]._id} name={list[i].name} description={list[i].description} complete={this.state.default} clicked={this.clicked} list={list[i].list} completechange={this.completechange}/>
+          let id2=list[i]._id
+          let id3 = id2.replace(/\D/g,'');
+          return <List key={i} id={list[i]._id} id2={id3} name={list[i].name} description={list[i].description} duedate={list[i].duedate} complete={this.state.default} clicked={this.clicked} list={list[i].list} completechange={this.completechange}/>
         })} catch (e) {
             console.log(e)
         }}
@@ -66,7 +85,9 @@ export default class Page extends Component {
             try{
             let list = this.props.list.filter(toDo => toDo.complete === true && toDo.list === listID)
             return list.map((item, i) => {
-              return <List key={i} id={list[i]._id} name={list[i].name} description={list[i].description} complete={list[i].complete} clicked={this.clicked} list={list[i].list} completechange={this.completechange}/>
+              let id2=list[i]._id
+              let id3 = id2.replace(/\D/g,'');
+              return <List key={i} id={list[i]._id} id2={id3} name={list[i].name} description={list[i].description} duedate={list[i].duedate} complete={list[i].complete} clicked={this.clicked} list={list[i].list} completechange={this.completechange}/>
             })} catch (e) {
                 console.log(e)
             }}
@@ -75,6 +96,7 @@ export default class Page extends Component {
             this.props.saveNew({
               name:this.state.name, 
               description:this.state.description,
+              duedate:this.state.duedate,
               list:this.state.list,
               complete: false,
             })
@@ -84,6 +106,7 @@ export default class Page extends Component {
             this.props.update({
               name:this.state.name, 
               description:this.state.description,
+              duedate: this.state.duedate,
               list: this.state.list,
               complete: this.state.complete,
               id: id
@@ -100,24 +123,39 @@ export default class Page extends Component {
                 </section>
               </div>
               <form>
-                  <label htmlFor="To Do">To Do:</label>
+                <h2>New To Do Item</h2>
+              <div className="field">
+                  <label htmlFor="To Do">Title:</label>
                   <input id = "To Do" type = "text" value={this.state.name} onChange={(e)=>{
                       this.setState({
                           name: e.target.value
                       })
                   }}/>
+                  </div>
+                  <div className="field">
                   <label htmlFor="Description">Description:</label>
                   <input id = "Description" type = "text" value={this.state.description} onChange={(e)=>{
                       this.setState({
                         description: e.target.value
                       })
                   }}/>
-                  <label htmlFor="list">List:</label>
+                  </div>
+                  <div className="field">
+                  <label htmlFor="due">Due Date:</label>
+                  <input id = "due" type = "text" value={this.state.duedate} onChange={(e)=>{
+                      this.setState({
+                        duedate: e.target.value
+                      })
+                  }}/>
+                  </div>
+                  <div className="field">
+                  <label htmlFor="list">List Name:</label>
                   <input id = "list" type = "text" value={this.state.list} onChange={(e)=>{
                       this.setState({
                         list: e.target.value
                       })
                   }}/>
+                  </div>
                   <input type="button" onClick={this.save} value="Submit"/>
               </form>
             </div>

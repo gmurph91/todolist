23e85ca0c6263f735cf6fa94417253dc7ef15f7d
greg <murphygreg6@gmail.com>
2020-03-12 21:04:46 -0600

    some css changes

--- src/components/list.js
+++ src/components/list.js
@@ -7,7 +7,10 @@ export default class List extends Component{
         return (
                 <div>
                     <button className="List" id={this.props.id2} onClick={this.props.clicked}>{this.props.name}</button>
+                    <label className="container">
                     <input name={this.props.name} data-description={this.props.description} data-duedate={this.props.duedate} id={this.props.id} data-list={this.props.list} type="checkbox" checked={this.props.complete} onChange={this.props.completechange}/>
+                    <span className="checkmark"></span>
+                    </label>
                     <div className="content" id={this.props.id2 + 1}>
                     <p>{this.props.description}</p>
                     <p>Due: {this.props.duedate}</p>
